492e90fcd3bb0baceb29ccbe50e6541a7d712bdb
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-04-10 21:00:12 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+  //contador começa em 0
+  const [count, setCount] = useState(0);
+  //funcao chamada para incrementar o botao
+  function incrementar(){
+    setCount(count + 1);
+  }
+  //funcao chamada para decrementar o botao
+  function decrementar(){
+    if(count > 0 )
+    setCount(count - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'> {/*classe CSS de estilo "App"*/}
+     <h1 className='Contador'>Contador</h1>
+      {count}
+      <div>
+      <button className = 'decrementar' onClick={decrementar}>-</button> 
+      <button className = 'incrementar' onClick={incrementar}>+</button> 
+     </div>
     </div>
   );
 }
