0f01a84b9507112faae1c38fbca4aa2d741317b4
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-04-17 00:28:23 -0300

    update

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel from 'react-bootstrap/Carousel';
 
-import image1 from '../img/1.jpg'
+//import image1 from '../img/1.jpg'
 import image2 from '../img/2.jpg'
 import image3 from '../img/3.jpg'
 
