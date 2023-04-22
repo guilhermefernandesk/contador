f78589c25381a1638624668327fd74e384d23420
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-04-22 15:10:08 -0300

    another-carousel

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { useState } from 'react';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import Carousel from 'react-bootstrap/Carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
-//import image1 from '../img/1.jpg'
-//carousel
+
+import galaxia from '../img/1.jpg'
 import mato from '../img/2.jpg'
 import casa from '../img/3.jpg'
+
+
+const slides = [
+    {
+        title: "galaxia",
+        retrato: galaxia,
+    },
+    {
+        title: "mato",
+        retrato: mato,
+    },
+    {
+        title: "casa",
+        retrato: casa,
+    }
+];
 
 function Contador({ history }) {
     //contador começa em 0
@@ -30,40 +46,30 @@ function Contador({ history }) {
 
     return (
         <div className='App'> {/*classe CSS de estilo "App"*/}
-            <div className="carousel1">
-                <Carousel
-                    slide={true}
-                    fade={true}
-                    controls={true}
-                    indicators={true}
-                    interval={3000}
-                    wrap={true} //reinicio
-                >
-                    <Carousel.Item>
-                        <img
-                            className="carousel" //controla altura
-                            src={mato}
-                            alt="mato One"
-                        />
-                        <Carousel.Caption>
-                            <h3>Primeira imagem</h3>
-                            <p>Texto da primeira imagem</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item >
-                        <img
-                            className="carousel" //controla altura
-                            src={casa}
-                            alt="casa Two"
-                        />
-                    </Carousel.Item>
-                </Carousel>
-            </div>
+            <Carousel
+                autoPlay={true}
+                interval={3000}
+                swipeable={true}
+                emulateTouch={true}
+                infiniteLoop={true}
+                showArrows={true}
+                showStatus={false}
+                showThumbs={false}
+            >
+                {slides.map((slide) => (
+                    <div key={slide.title}>
+                        <div className="image-wrapper">
+                            <img src={slide.retrato} />
+                            {slide.control}
+                        </div>
+                    </div>
+                ))}
+            </Carousel>
             <h1 className='Contador'>Contador</h1>
-            <div style={{fontSize:25}}> 
-            {count}
+            <div style={{ fontSize: 25 }}>
+                {count}
             </div>
-            <div style={{padding:15}}>
+            <div style={{ padding: 15 }}>
                 <button className='decrementar' onClick={decrementar} >-</button>
                 <button className='incrementar' onClick={incrementar}>+</button>
             </div>
