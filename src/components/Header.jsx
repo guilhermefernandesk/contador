fe905f1d1f05b1e757e9a3300168dd85cf83e064
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-05-04 08:10:02 -0300

    Header/Sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { useState } from 'react';
 
+import Sidebar from "./Sidebar";
 import '../components/Header.css'
 
-const Header = () => (
-    <div className="header">
-        
-        <a href="#" className="logo">Logo</a>
-        <div className="header-right">
-            <a href="#">Login</a>
+function Header() {
+    const [showSidebar, setShowSidebar] = useState(false);
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar);
+    };
+
+    return (
+        <div className="header">
+            <a href="#" className="btn">
+                <button onClick={toggleSidebar}>=</button>
+            </a>
+            <Sidebar showSidebar={showSidebar}/>
+            <a href="#" className="logo">Logo</a>
+            <div className="header-right">
+                <a href="#">Login</a>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default Header;
 
