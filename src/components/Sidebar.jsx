f0812cfe19dc8bb6c380e64a36af7baf5fe1c49b
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-05-05 20:15:49 -0300

    menu-sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,13 @@
+//yarn add @material-ui/core
+
+//yarn add @material-ui/icons
+
 import React from "react";
 import { useState } from 'react';
 
 import '../components/Sidebar.css'
+
+import SearchIcon from '@material-ui/icons/Search';
 
 const itensCheckbox = [
     {
@@ -63,6 +69,8 @@ function Sidebar({ showSidebar }) {
     const [showCamadasContainer, setShowCamadasContainer] = useState(false);
     const [showSobreContainer, setShowSobreContainer] = useState(false);
     const [showBuscaContainer, setShowBuscaContainer] = useState(false);
+    const [opcaoSelecionada, setOpcaoSelecionada] = useState('');
+
     const toggleCamadasContainer = () => {
         setShowCamadasContainer(!showCamadasContainer);
         setShowBuscaContainer(false);
@@ -74,10 +82,51 @@ function Sidebar({ showSidebar }) {
         setShowCamadasContainer(false); // oculta a outra div, se ela estiver aberta
     };
     const toggleBuscaContainer = () => {
-        setShowBuscaContainer(!showSobreContainer);
+        setShowBuscaContainer(!showBuscaContainer);
         setShowSobreContainer(false);
         setShowCamadasContainer(false); // oculta a outra div, se ela estiver aberta
     };
+
+    function handleOpcaoSelecionada(event) {
+        setOpcaoSelecionada(event.target.value);
+    }
+
+    function renderizarCampoDeEntrada() {
+        switch (opcaoSelecionada) {
+            case 'inscricao':
+                return (
+                    <div className="input-group" style={{ display: "flex" }}>
+                        <input className="form-control" type="text" placeholder="Inscrição Cadastral" />
+                        <button className="search-btn">
+                            <SearchIcon />
+                        </button>
+
+                    </div>
+                );
+            case 'n-reduzido':
+                return (
+                    <div className="input-group" style={{ display: "flex" }}>
+                        <input className="form-control" type="text" placeholder="Nº inscr. reduzido" />
+                        <button className="search-btn">
+                            <SearchIcon />
+                        </button>
+                    </div>
+                );
+            case 'endereco':
+                return (
+                    <div className="input-group" style={{ display: "flex" }}>
+                        <input className="form-control" type="text" placeholder="Logradouro" />
+                        <input className="form-control" type="text" placeholder="Endereço" />
+                        <button className="search-btn">
+                            <SearchIcon />
+                        </button>
+                    </div>
+                );
+            default:
+                return null;
+        }
+    }
+
 
 
     return (
@@ -106,18 +155,17 @@ function Sidebar({ showSidebar }) {
                     </li>
                     {showBuscaContainer && (
                         <div className="text-container">
-                            <div>
-                                <select>
-                                    <option value="tipo" selected disabled>Busca por...</option>
+                            <div className="input-group">
+                                <select value={opcaoSelecionada} onChange={handleOpcaoSelecionada}>
+                                    <option value="" selected disabled>Busca por...</option>
                                     <option value="inscricao">Inscrição Cadastral</option>
                                     <option value="n-reduzido">Nº inscr. reduzido</option>
                                     <option value="endereco">Endereço</option>
                                 </select>
                             </div>
+                            {renderizarCampoDeEntrada()}
                         </div>
                     )}
-
-
                     <li onClick={toggleSobreContainer}>
                         Sobre
                     </li>
