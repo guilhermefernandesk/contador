0deb68cc777ae1873edc92400e88f4f7005d5fba
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-05-17 20:17:16 -0300

    Revert "update"
    This reverts commit 1431d5f6d2456061f2fe2b531a0438527e6f2691.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,56 +5,12 @@ import '../components/Sidebar.css'
 
 const itensCheckbox = [
     {
-        id: 'area-especial',
+        id: 'area_especial',
         label: 'Área especial'
     },
     {
         id: 'bairros',
         label: 'Bairros'
-    },
-    {
-        id: 'benfeitoria',
-        label: 'Benfeitoria'
-    },
-    {
-        id: 'edificacao',
-        label: 'Edificação'
-    },
-    {
-        id: 'eixo',
-        label: 'Eixo'
-    },
-    {
-        id: 'hidrografia',
-        label: 'Hidrografia'
-    },
-    {
-        id: 'logradouro',
-        label: 'Logradouro'
-    },
-    {
-        id: 'lote',
-        label: 'Lote'
-    },
-    {
-        id: 'lote-selecionado',
-        label: 'Lote Selecionado'
-    },
-    {
-        id: 'mancha-urbana',
-        label: 'Mancha Urbana'
-    },
-    {
-        id: 'ponto-interesse',
-        label: 'Ponto de Interesse'
-    },
-    {
-        id: 'quadra',
-        label: 'Quadra'
-    },
-    {
-        id: 'setores',
-        label: 'Setores'
     },
 
 ];
@@ -62,20 +18,13 @@ const itensCheckbox = [
 function Sidebar({ showSidebar }) {
     const [showCamadasContainer, setShowCamadasContainer] = useState(false);
     const [showSobreContainer, setShowSobreContainer] = useState(false);
-    const [showBuscaContainer, setShowBuscaContainer] = useState(false);
+
     const toggleCamadasContainer = () => {
         setShowCamadasContainer(!showCamadasContainer);
-        setShowBuscaContainer(false);
         setShowSobreContainer(false); // oculta a outra div, se ela estiver aberta
     };
     const toggleSobreContainer = () => {
         setShowSobreContainer(!showSobreContainer);
-        setShowBuscaContainer(false);
-        setShowCamadasContainer(false); // oculta a outra div, se ela estiver aberta
-    };
-    const toggleBuscaContainer = () => {
-        setShowBuscaContainer(!showSobreContainer);
-        setShowSobreContainer(false);
         setShowCamadasContainer(false); // oculta a outra div, se ela estiver aberta
     };
 
@@ -101,30 +50,13 @@ function Sidebar({ showSidebar }) {
                             ))}
                         </div>
                     )}
-                    <li onClick={toggleBuscaContainer}>
-                        Busca Imóvel
-                    </li>
-                    {showBuscaContainer && (
-                        <div className="text-container">
-                            <div>
-                                <select>
-                                    <option value="tipo" selected disabled>Busca por...</option>
-                                    <option value="inscricao">Inscrição Cadastral</option>
-                                    <option value="n-reduzido">Nº inscr. reduzido</option>
-                                    <option value="endereco">Endereço</option>
-                                </select>
-                            </div>
-                        </div>
-                    )}
-
-
                     <li onClick={toggleSobreContainer}>
                         Sobre
                     </li>
-                    {showSobreContainer && (
-                        <div className="text-container">
-                            Projeto de Recadastramento Imobiliário
-                        </div>
+                    {showSobreContainer &&(
+                    <div className="text-container">
+                        Projeto de Recadastramento Imobiliário
+                    </div>
                     )}
                 </ul>
 
