f621d90a0a68c9d21c4388c10e2ea03c4d108652
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-05-17 20:17:53 -0300

    Revert "header with makeStyles"
    This reverts commit 6645fde8ff11d69bce6a36a6464a67e7a2e105eb.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,14 @@
 import React from "react";
-import { makeStyles } from "@mui/material";
 
-const Header = () => {
-    const classes = useStyles();
-
-    <div className={classes.header}>
-        <a href="#" className={`${classes.headerLink} ${classes.logo}`}>Logo</a>
-        <div className={classes.headerRight}>
+const Header = () => (
+    <div className="header">
+        
+        <a href="#" className="logo">Logo</a>
+        <div className="header-right">
             <a href="#">Login</a>
         </div>
     </div>
-}
+)
 
 export default Header;
 
-const useStyles = makeStyles({
-    headerLink: {
-      float: 'left',
-      color: 'black',
-      textAlign: 'center',
-      padding: 12,
-      textDecoration: 'none',
-      fontSize: 18,
-      lineHeight: 25,
-      borderRadius: 4,
-      '&.logo': {
-        fontSize: 25,
-        fontWeight: 'bold',
-      },
-      '&:hover': {
-        backgroundColor: '#ddd',
-        color: 'black',
-      },
-    },
-    headerRight: {
-      float: 'right',
-    },
-  });
