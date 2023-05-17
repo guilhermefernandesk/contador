6c364aa16393eb81f6b7ede9aab405a17e709372
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-05-17 20:17:55 -0300

    Revert "Typography-error-solved"
    This reverts commit 61d720248dd35169c7ff1aa609ffa570631d06ae.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,8 +11,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
-
+import Typography from '@mui/material/Typography';
 
 
 
