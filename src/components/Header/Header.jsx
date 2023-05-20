1c378029e98b4b44c1153d0a3dfc2f83455cc987
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-05-19 22:27:57 -0300

    solve-problem-href

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
     return (
         <header className="header-fixed">
             <div className="header-limiter">
-                <h1><a>Logo</a></h1>
+                <h1><a href="#">Logo</a></h1>
                 <nav>
-                    <a>HOME</a>
-                    <a>SOBRE</a>
-                    <a>SOBRE</a>
+                    <a href="#">HOME</a>
+                    <a href="#">SOBRE</a>
+                    <a href="#">SOBRE</a>
                 </nav>
             </div>
         </header>
