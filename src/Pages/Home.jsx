0b747a8f736c239befadc1635b6636f3921e1464
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-06-09 01:47:52 -0300

    radio button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,27 +1,61 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 
-import Header from "../components/Header/Header";
+import '../components/Header/Header.css'
+import { useState } from "react";
 
 
 
 function Home() {
   const { count } = useParams();
   const { name } = useParams();
+  const [opcaoSelecionada, setOpcaoSelecionada] = useState("Home");
 
   return (
     <div>
-      <div>
-        <Header />
+
+      <div className="sub-header">
+        <input
+          type="radio"
+          name="s1"
+          id="o1"
+          value="Home"
+          checked={opcaoSelecionada === "Home"}
+          onChange={(e) => setOpcaoSelecionada(e.target.value)}
+        />
+        <label for="o1">Home</label>
+        <input
+          type="radio"
+          name="s1"
+          id="o2"
+          value="Sobre"
+          checked={opcaoSelecionada === "Sobre"}
+          onChange={(e) => setOpcaoSelecionada(e.target.value)}
+        />
+        <label for="o2">Sobre</label>
       </div>
+
+
       <div>
-        <h1>Home Page</h1>
-        <p>Contador: {count} </p>
-        <p>Nome: {name} </p>
-        <Link to="/noticias">
-          <button>NEWS PAGE</button>
-        </Link>
+        {opcaoSelecionada === "Home" && (
+          <div>
+            <h1>Home Page</h1>
+            <p>Contador: {count} </p>
+            <p>Nome: {name} </p>
+            <Link to="/noticias">
+              <button>NEWS PAGE</button>
+            </Link>
+          </div>
+        )}
+
+        {opcaoSelecionada === "Sobre" && (
+          <div className="conteudo-lista">
+            <label>N° Inscrição</label>
+            <input id="nInscricao" name="nInscricao" />
+          </div>
+        )}
       </div>
+
     </div>
   );
 };
